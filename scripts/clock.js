a9c1fd6463df121ece075889344e528f1a905e48
Chris Hundhausen <hundhaus@wsu.edu>
2021-09-30 15:13:54 -0700

    Add FormattedDate component to display current date Add button to toggle date display on and off

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,3 +1,29 @@
+/*************************************************************************
+ * File: Clock.js
+ * This file defines the various clock and date components developed 
+ * in Chapter 10.
+ ************************************************************************/
+
+/*************************************************************************
+ * @class FormattedDate 
+ * @Desc 
+ * This React component renders the current date.
+ *************************************************************************/
+class FormattedDate extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return this.props.date.toLocaleDateString();
+  }
+}
+
+/*************************************************************************
+ * @class FormattedTime 
+ * @Desc 
+ * This React component renders the current time.
+ *************************************************************************/
 class FormattedTime extends React.Component {
   constructor(props) {
     super(props);
@@ -7,18 +33,30 @@ class FormattedTime extends React.Component {
   }
 }
 
+/*************************************************************************
+ * @class Clock 
+ * @Desc 
+ * This React component represents a digital clock that displays the 
+ * current time and date.
+ *************************************************************************/
 class Clock extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {date: new Date()};
+        this.state = {
+          date: new Date(),
+          displayDate: false};
     }
 
     updateTime() {
         this.setState({
           date: new Date()
         });
-      }
+    }
 
+    buttonClick = () => {
+        this.setState({displayDate: !this.state.displayDate});
+    }
+      
 
     componentDidMount() {
         this.timer = setInterval(
@@ -35,11 +73,18 @@ class Clock extends React.Component {
       return (
         <div>
           <h1>Hello React Students!</h1>
-          <h2>It is <FormattedTime date={this.state.date} />. </h2>
+          {this.state.displayDate ?
+            <h2>The date is <FormattedDate date={this.state.date} />.</h2>
+            : null}
+          <h2>The time is <FormattedTime date={this.state.date} />.</h2>
+          <button className="btn btn-primary" onClick={this.buttonClick}>
+              {this.state.displayDate ? "Hide Date" : "Show Date"}
+          </button>
         </div>
       );
     }
-}
+  }
+
 
 ReactDOM.render(
     <Clock />,
