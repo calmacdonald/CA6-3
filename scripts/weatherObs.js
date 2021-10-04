5dc34cb03a0f47184539ba9556dab7c8cd483c1b
Chris Hundhausen <hundhaus@wsu.edu>
2021-10-04 11:46:09 -0700

    Add first version of click handler to add weather station

--- scripts/weatherObs.js
+++ scripts/weatherObs.js
@@ -16,6 +16,21 @@ class WeatherObs extends React.Component {
         this.setState({station: {lat: 47.61, long: -122.33}});
     }
 
+    addStation = async() => {
+        const newStation = prompt("Enter a City, State, and Country:");
+        if (newStation != null) { //Need to see if we can find the station through the API 
+          const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=' +
+              newStation +  '&appid=98cb8d2538da248784d8e1c1f9332ea9');
+          const stationData = await response.json();
+          //See if the requested station exists
+          if (stationData != null && stationData.hasOwnProperty('coord')) { 
+              //TO DO: Add station to list
+          } else { 
+              alert("Sorry, that weather location could not be found.");
+          }
+       }
+    }
+    
     componentDidMount = () => {
         //Initialize based on user's current location, if possible
         navigator.geolocation.getCurrentPosition(this.getLocSuccess,this.getLocError);
